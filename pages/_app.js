0d4ccf3c354175d591a15b513c46d10f93163212
vugomars <vugomars@gmail.com>
2022-03-17 06:33:14 +0700

    Fix _App.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,27 +8,27 @@ import '@styles/globals.css'
 const Noop = ({ children }) => <>{children}</>
 
 function MyApp({ Component, pageProps }) {
-  const Layout = Component.Layout ?? Noop
+    const Layout = Component.Layout ?? Noop
 
-  return (
-    <Web3Provider>
-      <Head>
-        <meta charSet="utf-8" />
-        <title>BunTech - Blockchain Blog</title>
-        <meta name="theme-color" content="#000000" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon.png" />
-      </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-      <ToastContainer
-        hideProgressBar
-        position="bottom-right"
-        autoClose={2000}
-      />
-    </Web3Provider>
-  )
+    return (
+        <Web3Provider>
+            <Head>
+                <meta charSet="utf-8" />
+                <title>BunTech - Blockchain Blog</title>
+                <meta name="theme-color" content="#000000" />
+                <link rel="shortcut icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon.png" />
+            </Head>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+            <ToastContainer
+                hideProgressBar
+                position="bottom-right"
+                autoClose={2000}
+            />
+        </Web3Provider>
+    )
 }
 
 export default MyApp
